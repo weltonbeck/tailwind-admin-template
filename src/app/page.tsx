1ddd2ff974a67c6faae45464f0969631d642fdfe
weltonbeck <tomwb1@gmail.com>
2024-01-19 17:06:44 -0300

    criar um componente de exemplo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+'use client'
+import { TodoList } from '@/components/todoList'
+
 export default function Page() {
   return (
-    <div className="flex h-screen items-center justify-center bg-gray-800">
+    <div className="flex h-screen flex-col items-center justify-center bg-gray-800">
       <h1 className="text-3xl font-black text-white">Hello world!</h1>
+      <div className="pt-4">
+        <TodoList itens={['exemplo', 'configurar projeto']} />
+      </div>
     </div>
   )
 }
---
+++ src/components/todoList/index.tsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+
+type TodoListProps = {
+  itens: string[]
+}
+
+export function TodoList({ itens }: TodoListProps) {
+  const [list, setList] = useState(itens)
+  const [inputValue, setInputValue] = useState('')
+
+  const addNew = () => {
+    if (inputValue) {
+      setList([...list, inputValue])
+      setInputValue('')
+    }
+  }
+
+  return (
+    <>
+      <div className="pb-4">
+        <input
+          type="text"
+          className="mr-2 rounded-sm border-2 border-gray-300 p-1 px-2 font-light outline-none focus:border-blue-500"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          placeholder="Digite aqui"
+        />
+        <button
+          className="rounded bg-blue-500 px-4 py-2 font-medium text-white transition hover:bg-blue-700"
+          onClick={addNew}
+        >
+          Adicionar Novo
+        </button>
+      </div>
+      <ul className="list-disc text-white">
+        {list.map((value, index) => (
+          <li key={index}>{value}</li>
+        ))}
+      </ul>
+    </>
+  )
+}
